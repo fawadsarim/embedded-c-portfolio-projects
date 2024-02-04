ade505cfc57bf37e1b685cd34d962faec00d7c2b
Muhammad Fawad Ul Hassan Sarim <info.fawadsarim@gmail.com>
2024-02-04 19:40:23 +0500

    Arduino Project Efficient Memory Utilization
    In this basic project on Arduino Uno, I performed basic optimization on code. There are two folders, one with unoptimized code and other one optimized code. Code was optimized using suitable datatypes for efficient memory utilization.

---
+++ 2_basic_memory_optimization_arduino/memory_usage/memory/memory.ino
@@ -0,0 +1,13 @@
+#define SIZE (1000)
+
+int x;
+int sensor_data[SIZE];
+
+void setup(){
+}
+
+void loop(){
+  sensor_data[x] = x;
+  x = x<SIZE-1 ? x+1 : 0;
+  digitalWrite(LED_BUILTIN,sensor_data[x]);
+}
